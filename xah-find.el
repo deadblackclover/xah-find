77d627a53f6ba179c57d8213e316997889197de2
Xah Lee <xah@xahlee.org>
2017-08-11 21:22:49 -0700

    added patreon to readme

--- xah-find.el
+++ xah-find.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2012-2017 by Xah Lee
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
-;; Version: 3.1.3
+;; Version: 3.1.4 2017-08-11
 ;; Created: 02 April 2012
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: convenience, extensions, files, tools, unix
